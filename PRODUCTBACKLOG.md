8229b6e6de1dc1e6142d0a60f90ad2776270e66c
Leo P <leo.projceski@education.nsw.gov.au>
2026-06-03 13:00:29 +1000

    Revert "Housekeeping"

--- PRODUCTBACKLOG.md
+++
@@ -1,8 +0,0 @@
-# **Product Backlog**
-
-| ID            | User Story    | Priority      | Acceptance Criteria | Status        |
-| ------------- | ------------- | ------------- | ------------------- | ------------- |
-| Row 1, Cell 1 | Row 1, Cell 2 | Row 1, Cell 3 | Row 1, Cell 1       | Row 1, Cell 2 |
-| Row 2, Cell 1 | Row 2, Cell 2 | Row 2, Cell 3 | Row 2, Cell 1       | Row 2, Cell 2 |
-
-## Changelog
--- userManagement.py
+++ userManagement.py
@@ -1,51 +1,12 @@
 import sqlite3 as sql
 import bcrypt
-import pyotp
 
 
-def getUsers(email, password):
+### example
+def getUsers():
     con = sql.connect("databaseFiles/database.db")
     cur = con.cursor()
-    cur.execute("SELECT password FROM user_info WHERE email = ?", (email,))
-    result = cur.fetchone()
+    cur.execute("SELECT * FROM id7-tusers")
     con.close()
-    if result is None:
-        return False
-    stored_password = result[0]
-    password_bytes = password.encode("utf-8")
-    if isinstance(stored_password, str):
-        stored_password = stored_password.encode("utf-8")
+    return cur
 
-    return bcrypt.checkpw(password_bytes, stored_password)
-
-
-def insertSignup(email, password):
-    con = sql.connect("databaseFiles/database.db")
-    cur = con.cursor()
-
-    password_bytes = password.encode("utf-8")
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    hashed_password_str = hashed_password.decode("utf-8")
-    totp_secret = pyotp.random_base32()
-
-    try:
-        cur.execute(
-            "INSERT INTO user_info (email, password, totp_secret) VALUES (?,?,?)",
-            (email, hashed_password_str, totp_secret),
-        )
-        con.commit()
-        con.close()
-        return True
-    except sql.IntegrityError:
-        con.close()
-        return False
-
-
-def getUserSecret(email):
-    con = sql.connect("databaseFiles/database.db")
-    cur = con.cursor()
-    cur.execute("SELECT totp_secret FROM user_info WHERE email = ?", (email,))
-    result = cur.fetchone()
-    con.close()
-    return result[0] if result else None
